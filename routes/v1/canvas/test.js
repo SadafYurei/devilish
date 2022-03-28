175d103ad7efbef1ace6579bc81cc67ea2e74f9c
SadafYurei <75583903+SadafYurei@users.noreply.github.com>
2022-03-27 20:57:05 -0500

    Update test.js

--- routes/v1/canvas/test.js
+++ routes/v1/canvas/test.js
@@ -10,7 +10,7 @@ router.get('/', (req, res) => {
 		});
 	};
 try{
-    const base = `https://devilish-api.herokuapp.com/api/v1/canvas/changemymind?avatar=${image}`
+    const base = `https://devilish-api.herokuapp.com/api/v1/canvas/changemymind?avatar=${avatar}`
     res.json({url: base})
 }catch(error){ 
 res.send({error: error})	
