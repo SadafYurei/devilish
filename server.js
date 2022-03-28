b07df48276e93cd5812865830e13275c231eb222
SadafYurei <andrewgorajec@gmail.com>
2022-03-27 20:17:47 -0500

    lol

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const app = require('./app');
 const settings = require('./config/settings');
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 5000, () => {
 	console.log('Test Server Is Ready')
 });
